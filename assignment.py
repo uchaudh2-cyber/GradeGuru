fb24ba05a84100ae44348194eadae8a4b7aea9c6
Cynosuraee <148014123+Cynosuraee@users.noreply.github.com>
2025-11-13 20:53:08 -0500

    updated menu.py

--- assignment.py
+++ assignment.py
@@ -5,10 +5,35 @@ class Assignment:
         self.score = float(score)
         self.weight = float(weight)
 
+    def main_menu():
+            print(" Student Grade Manager")
+            print(" 1. Add Course")
+            print(" 2. Add assignment")
+            print(" 3. View Courses")
+            print(" 4. Exit")
+
+            choice = input("Select an option (1-4): ").strip()
+
+            if choice == "1":
+                print("Add Course selected")
+                # TODO: add the functionality
+            elif choice == "2":
+                print("Add Assignment selected")
+                # TODO: add the functionality
+            elif choice == "3":
+                print("View Courses Selected")
+                # TODO: add the functionality
+            elif choice == "4":
+                print("Exiting program")
+                # TODO: add the functionality
+            else:
+                print("Invalid input, please try again.")
+                # TODO: loop it to select choice again
+                
+    
     def get_weighted_score(self):
         """Returns weighted contribution (score * weight)."""
         return self.score * self.weight
 
     def __repr__(self):
         return f"{self.name}: score={self.score}, weight={self.weight}"
-#test
